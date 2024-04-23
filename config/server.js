68a4ea2c72ecd3626f504a547e205ab393d759f0
Leonard <lordknight1904@gmail.com>
2024-04-23 21:08:14 +0900

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1b890975aef1f59dd65cd57ebac97ab9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5626ae58-387c-4e46-a9df-5979e918ea83'
+};
